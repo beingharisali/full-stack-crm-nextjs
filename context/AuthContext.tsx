7254be99e0774fcc5ae911bf883d7734c55e2973
Shahzaib Fayyaz <shahzaibdev797@gmail.com>
2025-11-11 12:29:52 +0500

    added paggination and property list created

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -3,133 +3,132 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import {
-  register as registerApi,
-  login as loginApi,
-  // getProfile,
-  logoutApi,
+	register as registerApi,
+	login as loginApi,
+	// getProfile,
+	logoutApi,
 } from "../services/auth.api";
 import type { User, UserRole } from "../types/user";
 
 interface AuthContextType {
-  user: User | null;
-  loading: boolean;
-  registerUser: (
-    firstName: string,
-    lastName: string,
-    email: string,
-    password: string,
-    role: UserRole
-  ) => Promise<void>;
-  loginUser: (email: string, password: string, role: UserRole) => Promise<void>;
-  logoutUser: () => Promise<void>;
-  // refreshProfile: () => Promise<void>;
+	user: User | null;
+	loading: boolean;
+	registerUser: (
+		firstName: string,
+		lastName: string,
+		email: string,
+		password: string,
+		role: UserRole
+	) => Promise<void>;
+	loginUser: (email: string, password: string, role: UserRole) => Promise<void>;
+	logoutUser: () => Promise<void>;
+	// refreshProfile: () => Promise<void>;
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export function useAuthContext() {
-  const ctx = useContext(AuthContext);
-  if (!ctx) throw new Error("useAuthContext must be used inside AuthProvider");
-  return ctx;
+	const ctx = useContext(AuthContext);
+	if (!ctx) throw new Error("useAuthContext must be used inside AuthProvider");
+	return ctx;
 }
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
-  const [user, setUser] = useState<User | null>(null);
-  const [loading, setLoading] = useState(true);
-  const router = useRouter();
+	const [user, setUser] = useState<User | null>(null);
+	const [loading, setLoading] = useState(true);
+	const router = useRouter();
 
-  const getRedirectPath = (role?: string) => {
-    switch (role) {
-      case "admin":
-        return "/admin/dashboard";
-      case "agent":
-        return "/agent/dashboard";
-      case "user":
-        return "/user/dashboard";
-      default:
-        return "/";
-    }
-  };
+	const getRedirectPath = (role?: string) => {
+		switch (role) {
+			case "admin":
+				return "/admin/dashboard";
+			case "agent":
+				return "/agent/dashboard";
+			case "user":
+				return "/user/dashboard";
+			default:
+				return "/";
+		}
+	};
 
-  // const refreshProfile = async (): Promise<void> => {
-  //   try {
-  //     const res = await getProfile();
-  //     if (res?.user) {
-  //       setUser(res.user);
-  //     } else {
-  //       setUser(null);
-  //     }
-  //   } catch {
-  //     setUser(null);
-  //   }
-  // };
+	// const refreshProfile = async (): Promise<void> => {
+	//   try {
+	//     const res = await getProfile();
+	//     if (res?.user) {
+	//       setUser(res.user);
+	//     } else {
+	//       setUser(null);
+	//     }
+	//   } catch {
+	//     setUser(null);
+	//   }
+	// };
 
-  // useEffect(() => {
-  //   (async () => {
-  //     setLoading(true);
-  //     const token = localStorage.getItem("token");
+	// useEffect(() => {
+	//   (async () => {
+	//     setLoading(true);
+	//     const token = localStorage.getItem("token");
 
-  //     if (token) {
-  //       await refreshProfile();
-  //     }
-  //     setLoading(false);
-  //   })();
-  // }, []);
+	//     if (token) {
+	//       await refreshProfile();
+	//     }
+	//     setLoading(false);
+	//   })();
+	// }, []);
 
-  const registerUser = async (
-    firstName: string,
-    lastName: string,
-    email: string,
-    password: string,
-    role: UserRole
-  ) => {
-    const res = await registerApi(
-      firstName,
-      lastName,
-      email,
-      password,
-      role as string
-    );
+	const registerUser = async (
+		firstName: string,
+		lastName: string,
+		email: string,
+		password: string,
+		role: UserRole
+	) => {
+		const res = await registerApi(
+			firstName,
+			lastName,
+			email,
+			password,
+			role as string
+		);
 
-    if (res.token) {
-      localStorage.setItem("token", res.token);
-    }
+		if (res.token) {
+			localStorage.setItem("token", res.token);
+		}
 
-    setUser(res.user);
-    console.log("res.user", res);
+		setUser(res.user);
+		console.log("res.user", res);
 
-    router.replace(getRedirectPath(res.user.role));
-  };
-  const loginUser = async (email: string, password: string, role: UserRole) => {
-    const res = await loginApi(email, password, role as string);
+		router.replace(getRedirectPath(res.user.role));
+	};
+	const loginUser = async (email: string, password: string, role: UserRole) => {
+		const res = await loginApi(email, password, role as string);
 
-    if (res.token) {
-      localStorage.setItem("token", res.token);
-    }
+		if (res.token) {
+			localStorage.setItem("token", res.token);
+		}
 
-    setUser(res.user);
+		setUser(res.user);
 
-    router.replace(getRedirectPath(res.user.role));
-  };
+		router.replace(getRedirectPath(res.user.role));
+	};
 
-  const logoutUser = async () => {
-    await logoutApi();
-    setUser(null);
-    router.replace("/");
-  };
+	const logoutUser = async () => {
+		await logoutApi();
+		setUser(null);
+		router.replace("/");
+	};
 
-  return (
-    <AuthContext.Provider
-      value={{
-        user,
-        loading,
-        registerUser,
-        loginUser,
-        logoutUser,
-        // refreshProfile,
-      }}
-    >
-      {children}
-    </AuthContext.Provider>
-  );
+	return (
+		<AuthContext.Provider
+			value={{
+				user,
+				loading,
+				registerUser,
+				loginUser,
+				logoutUser,
+				// refreshProfile,
+			}}>
+			{children}
+		</AuthContext.Provider>
+	);
 }
