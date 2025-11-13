ffb20833902a4f3bdd7cf36f50ca4ccf30f8ab67
Maroof <maroofahamd076@gamil.com>
2025-11-13 19:06:58 +0500

    sidebar code change and navbar and sidebar added in properties listen page

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/context/AuthContext";
+import Navbar from "./components/navbar";
+import Sidebar from "./components/sidebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,11 @@ export default function RootLayout({
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <AuthProvider>{children}</AuthProvider>
+        <AuthProvider>
+          {/* <Navbar /> */}
+          {/* <Sidebar /> */}
+          {children}
+        </AuthProvider>
       </body>
     </html>
   );
