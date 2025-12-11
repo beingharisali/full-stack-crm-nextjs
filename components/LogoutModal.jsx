9857b614c5957aabdbc7f59fd6e3eec2a293be90
Fatima <bc220417002fzn@vu.edu.pk>
2025-12-11 13:40:45 +0500

    testing

--- components/LogoutModal.jsx
+++ components/LogoutModal.jsx
@@ -11,6 +11,8 @@ const LogoutModal = ({ isOpen, onClose, onConfirm }) => {
     }
   };
 
+  console.log("testing");
+
   return (
     <div 
       className="fixed inset-0 bg-black/30 backdrop-blur-sm flex items-center justify-center z-50 p-4"
